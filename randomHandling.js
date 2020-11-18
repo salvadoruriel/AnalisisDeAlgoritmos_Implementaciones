994bd7fa780a127ce35b59323c857ddbd6144afe
Salvador Aguirre <salvador.urielgm@gmail.com>
2020-11-18 08:13:46 +0000

    improved randomHandling speed + benchmarks

--- randomHandling.js
+++ randomHandling.js
@@ -2,17 +2,19 @@ export const makeRandomList = () =>{
   var t0 = performance.now();
 
   var nNums = parseInt(document.getElementById('nNums').value,10);
-  var Arr = []
-  //var minVal = Number.MIN_SAFE_INTEGER;
-  //var maxVal = Number.MAX_SAFE_INTEGER;
+  var Arr = new Array(nNums)
   var minVal = 0;
   var maxVal = nNums*3;
   for(var i =0; i< nNums; i++){
-    //Entero con min incluido y max excluido
-    Arr.push(Math.floor(Math.random() * (maxVal-minVal) ) + minVal);
+    Arr[i] = Math.floor(Math.random() * (maxVal-minVal) ) + minVal;
   }
   document.getElementById('f_numeros').value = Arr.toString(); 
 
   var t1 = performance.now();
-  console.log('Time Rand: ' + (t1-t0) );
+  console.log('Time Rand2: ' + (t1-t0) );
 }
+/*comparacion con nNums = 999999
+https://jsben.ch/G6Wc9
+//con nNums = 42000
+https://jsben.ch/Jejq1
+*/
